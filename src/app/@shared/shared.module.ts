4037ff9ba598c94f6eaa0addee559de57e541bbb
jorj-tsere <jorj.tsere@gmail.com>
2021-04-18 00:46:15 +0400

    save sss

--- src/app/@shared/shared.module.ts
+++ src/app/@shared/shared.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FooterComponent } from './layout/footer/footer.component';
+import { FooterComponent } from './footer/footer.component';
 import { MaterialModule } from './material/material.module';
-import { HeaderModule } from './layout/header/header.module';
+import { HeaderModule } from './header/header.module';
 import { LayoutComponent } from './layout/layout.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { DateMenuComponent, SettingsMenuComponent } from './ui-elements';
