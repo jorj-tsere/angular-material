26bf7fb61f26ef14f2e6047908011e68efe915b1
jorj-tsere <jorj.tsere@gmail.com>
2021-04-20 12:59:07 +0400

    save changes

--- src/app/pages/auth/services/auth.service.ts
+++ src/app/pages/auth/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of  } from 'rxjs';
+import axios from 'axios';
+import { Observable, of } from 'rxjs';
 import { ILoginCredentials } from '../models/login-credentials';
 import { IRegisterRequest } from '../models/register-request';
 import { IUser } from '../models/user';
@@ -9,30 +10,69 @@ import { IUser } from '../models/user';
   providedIn: 'root',
 })
 export class AuthService {
-  // baseUrl: string = 'http://localhost:3000';
   // tslint:disable-next-line:no-inferrable-types
-  baseUrl: string = 'http://68.183.115.254:8081';
+  baseUrl: string = 'http://68.183.115.254:8081'; // 'http://127.0.0.1:4200';
+  // tslint:disable-next-line:no-inferrable-types
+  // baseUrl: string = 'http://68.183.115.254:8081';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   login(credentials: ILoginCredentials): Observable<any> {
-    
-    const header = new HttpHeaders();
-    header.set('Access-Control-Allow-Origin', '*');
-    header.set('asdas', 'sada');
+
+    axios('http://68.183.115.254:8081/api/auth/getAccessToken', {
+      method: 'POST',
+      data: credentials,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Content-Type': 'application/json',
+      },
+      withCredentials: ,
+      credentials: 'same-origin',
+    }).then(response => {
+    })
+
+    axios
+      .post('http://68.183.115.254:8081/api/auth/getAccessToken', credentials, {
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+      })
+      .then((resp) => console.log)
+      .catch((error) => console.log);
+
+    const observable$ = Observable.create((observer) => {
+      axios
+        .get('https://jsonplaceholder.typicode.com/users')
+        .then((response) => {
+          observer.next(response.data);
+          observer.complete();
+        })
+        .catch((error) => {
+          observer.error(error);
+        });
+    });
+    return observable$;
+    // axios.post(this.baseUrl + '/api/auth/getAccessToken', credentials);
+
+    // let observable$ = Observable.create((observer) => {});
+
+    // return observable$;
+
     // const body = JSON.stringify(
     // {
-    // title: "data" 
+    // title: "data"
     // });
     // headers.append('Access-Control-Allow-Origin', '*');
 
-
-    return this.http.post(this.baseUrl + '/api/auth/getAccessToken', credentials, { headers: header })
+    // return this.http.post(this.baseUrl + '/api/auth/getAccessToken', credentials, { headers: header })
   }
 
   register(registerPayload: IRegisterRequest): Observable<any> {
-    return this.http
-      .post(this.baseUrl + '/api/adminRegistration/register', registerPayload);
+    return this.http.post(
+      this.baseUrl + '/api/adminRegistration/register',
+      registerPayload
+    );
   }
 
   public getUser(): Observable<IUser> {
--- src/app/services/base-api.service.ts
+++ src/app/services/base-api.service.ts
@@ -1,13 +1,60 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+// import { NotificationService } from '@crystal-one-services/notification.service';
+// import requestPromise from 'request-promise';
+// import { fetch } from 'isomorphic-fetch';
+// import {fetch as fetchPolyfill} from 'whatwg-fetch';
+// const requestPromise = require('request-promise');
 
-@Injectable({
-  providedIn: 'root',
-})
-export class BaseAPIService {
-  constructor(public http: HttpClient) {}
+import axios from 'axios';
 
-  get(url: string) {
-    return this.http.get(url);
-  }
+export async function baseService<T = any>(method: string, url: string, data?: any, header?: Headers, hostname?: string) {
+
+    const option: any = {
+        withCredentials: true,
+        // mode: 'cors',
+        method: method,
+        url: hostname ? hostname + url : localStorage.getItem('baseApiUrl') + url,
+        headers: {
+           Accept: 'application/json',
+        },
+    };
+
+    if (header) {
+        option.headers = header;
+    }
+
+    if (data instanceof FormData) {
+        option.data = data;
+    } else if (typeof data === 'object') {
+        const formdata = new FormData();
+        formdata.append('json_post', JSON.stringify(data));
+        option.data = formdata;
+    }
+
+    let response = await axios(option)
+        .then(res => {
+            return res.data as Promise<T>;
+        }).catch((error) => {
+            if (error.response) {
+                // console.log('Error Response: ', error.response);
+                error.myBody = error.response.data;
+                if (error.response.status === 401) {
+                    // alert(error.myBody);
+                    location.replace('/');
+                }
+                throw error;
+            } else if (error.request) {
+                // The request was made but no response was received
+                // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+                // http.ClientRequest in node.js
+                error.myBody = 'The request was made but no response was received';
+                // console.log('XHR response was not received. URL: ', (option.url.slice(BaseApiUrl.length, option.url.length) || ''));
+                throw error;
+            } else {
+                // Something happened in setting up the request that triggered an Error
+                console.log('Error Message: ', error.message);
+            }
+            throw error;
+        });
+
+    return response;
 }
